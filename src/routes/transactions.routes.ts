60693dc1ed9378a8c246fb1d7b80a9d8d95dd736
Alison Veras <alisson_veras@hotmail.com>
2020-07-20 01:59:39 -0300

    Deleting a transaction

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -2,7 +2,7 @@ import { Router, json } from 'express';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
@@ -27,7 +27,10 @@ transactionsRouter.post('/', async (request, response) => {
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const deleteTransactionService = new DeleteTransactionService();
+  await deleteTransactionService.execute(id);
+  return response.status(204).send();
 });
 
 transactionsRouter.post('/import', async (request, response) => {
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,9 +1,16 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transacationRepo = getCustomRepository(TransactionsRepository);
+    const transaction = await transacationRepo.findOne({ where: { id } });
+    if (!transaction) {
+      throw new AppError('Transacation not found', 400);
+    }
+    await transacationRepo.remove(transaction);
   }
 }
-
 export default DeleteTransactionService;
